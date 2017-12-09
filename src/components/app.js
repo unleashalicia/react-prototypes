43b5190bc8196b351432fe217e8ca38eda059327
unleashalicia <alicia.adams.evans@gmail.com>
2017-12-09 15:32:04 -0800

    cleaning up branches

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,10 @@
-
-
+// import React from 'react';
+//
+// const App = () => (
+//     <div>
+//         <h1>My First React Functional Component</h1>
+//     </div>
+// )
+//
+// export default;
+//
--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const luckyNumber = () => Math.floor(Math.random()*1000)+1;
+
+// function luckyNumber(){
+//     const randomNumber = Math.floor(Math.random()*1000)+1;
+//     return randomNumber;
+// }
+
+const user = {
+    name: "Alicia",
+    luckyNumber: luckyNumber()
+};
+
+const Greeting = props => {
+    let name = props.person.name;
+    let luckyNumber = props.person.luckyNumber;
+    const newDiv = <div>
+        <h1>Hello, {name}.</h1>
+        <h2 className="text-muted">Your lucky number is {luckyNumber}!</h2>
+    </div>;
+    return newDiv;
+};
+
+// function Greeting(props){
+//     let name = props.person.name;
+//     let luckyNumber = props.person.luckyNumber;
+//     const newDiv = <div>
+//         <h1>Hello, {name}.</h1>
+//         <h2 className="text-muted">Your lucky number is {luckyNumber}!</h2>
+//     </div>;
+//     return newDiv;
+// }
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    <Greeting person={user}/>,
     document.getElementById('root')
 );
