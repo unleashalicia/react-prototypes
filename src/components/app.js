8b656c0851e84f3b97c5acdbf247bad766ac69cf
unleashalicia <alicia.adams.evans@gmail.com>
2017-12-10 16:45:28 -0800

    Stop watch has start, stop, elapsed, and reset methods

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,11 @@
+import React from 'react';
+import Stopwatch from './stopwatch';
 
+const App = () => (
+    <div className="container">
+        <h1 className="text-center">Stopwatch</h1>
+        <Stopwatch/>
+    </div>
+)
 
+export default App;
