dbf92bb0b42f743b4b7e568a780b6ad3aa0b52c1
unleashalicia <alicia.adams.evans@gmail.com>
2017-12-12 20:56:47 -0800

    At a bug

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ContactList from './contact_list';
 
 const App = () => (
     <div className="container">
         <h1 className="text-center">Address Book</h1>
+        <ContactList/>
     </div>
 );
 
