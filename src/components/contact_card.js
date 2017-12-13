a383970857f1435b28e18c0a648c7a5e78446d47
unleashalicia <alicia.adams.evans@gmail.com>
2017-12-13 12:41:38 -0800

    Finished Feature Set 2

--- src/components/contact_card.js
+++ src/components/contact_card.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
-export default props => (
+const ContactCard =  props => (
     <h1>Full Name: {props.firstName} {props.lastName}</h1>
-)
+);
+
+export default ContactCard;
--- src/components/contact_list.js
+++ src/components/contact_list.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import ContactCard from ''
+import ContactCard from './contact_card';
+import ContactData from '../data/contact';
 
 class ContactList extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            contacts: ContactData
+        }
+
     }
 
     render() {
-        return <h1>Contact List</h1>
+        const list = this.state.contacts.map((item, index)=>{return <ContactCard key={index} firstName={item.firstName} lastName={item.lastName}/>});
+        return <div>{list}</div>;
     }
 }
 
