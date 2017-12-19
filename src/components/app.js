5f8e03ef24a350f5290db6c72b7b4e14a62471e1
unleashalicia <alicia.adams.evans@gmail.com>
2017-12-18 19:12:26 -0800

    Feature Set 2

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Welcome from './welcome';
+import {Route} from 'react-router-dom';
 
 const App = () => {
     return (
         <div>
             <h1>Routing!</h1>
+            <Route exact path="/" component={Welcome}/>
         </div>
     )
 };
---
+++ src/components/welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link, Route} from 'react-router-dom';
+
+const Welcome = () => {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <p>This is a routing demo.</p>
+        </div>
+    );
+};
+
+export default Welcome;
