e01e7d41ea22a7e39f54b350925fab6418cb3dad
unleashalicia <alicia.adams.evans@gmail.com>
2017-12-18 20:43:48 -0800

    Complete

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
+import './nav.css';
 
 const Nav = () => {
     return (
       <ul className="nav nav-tabs mt-3">
           <li className="nav-item">
-              <Link className="nav-link" to="/">Welcome</Link>
+              <NavLink activeClassName="active selected" className="nav-link" exact to="/">Welcome</NavLink>
           </li>
           <li className="nav-item">
-              <Link className="nav-link" to="/our-macarons">Our Macarons</Link>
+              <NavLink activeClassName="active selected" className="nav-link" to="/our-macarons">Our Macarons</NavLink>
           </li>
           <li className="nav-item">
-              <Link className="nav-link" to="/gifts-and-parties">Gifts &amp; Parties</Link>
+              <NavLink activeClassName="active selected" className="nav-link" to="/gifts-and-parties">Gifts &amp; Parties</NavLink>
           </li>
           <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact</Link>
+              <NavLink activeClassName="active selected" className="nav-link" to="/contact">Contact</NavLink>
           </li>
       </ul>
     );
