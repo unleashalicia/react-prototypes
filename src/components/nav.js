671ffc1c6e5ded263d741516ae083f62ab2eb601
unleashalicia <alicia.adams.evans@gmail.com>
2017-12-18 19:18:07 -0800

    Working on Feature Set 3

---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Nav = () => {
+    return (
+      <ul className="nav nav-tabs mt-3">
+          <li className="nav-item"></li>
+      </ul>
+    );
+};
+
+export default Nav;
