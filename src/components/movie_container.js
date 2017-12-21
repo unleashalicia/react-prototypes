3a7137c3dcd212fdf5f01e9b210e41642aa2467b
unleashalicia <alicia.adams.evans@gmail.com>
2017-12-21 14:19:40 -0800

    Feature set 2

--- src/components/movie_container.js
+++ src/components/movie_container.js
@@ -5,16 +5,23 @@ import axios from 'axios';
 class MovieContainer extends Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            movies: []
+        }
     }
 
     componentDidMount(){
         const BASE_URL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json";
         axios.get(BASE_URL).then((resp)=>{
-            console.log('RESP: ', resp);
+            this.setState({
+                movies: resp.data.feed.entry
+            });
         });
     }
 
     render(){
+        console.log(this.state);
         return (
             <div>
                 <h2>Movie Container</h2>
